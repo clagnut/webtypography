256818bb992ca289bb389970613c3cffdf101ccb
Richard Rutter <rich@clearleft.com>
2015-05-15 09:12:12 +0100

    correct link to Fontdeck

--- includes/headlinks.inc.php
+++ includes/headlinks.inc.php
@@ -3,4 +3,5 @@
 <!--[if lte IE 7]>
 <link rel="stylesheet" type="text/css" href="/css/ie-lte-7.css" />
 <![endif]-->
-<link rel="stylesheet" href="http://fontdeck.com/s/css/p+zwVqq4agQiKsiAkUNVfzsNquo/<?php echo $_SERVER['SERVER_NAME']; ?>/1575.css" type="text/css" />
+<link rel="stylesheet" href="//f.fontdeck.com/s/css/p+zwVqq4agQiKsiAkUNVfzsNquo/<?php echo $_SERVER['SERVER_NAME']; ?>/1575.css"
+type="text/css" />
